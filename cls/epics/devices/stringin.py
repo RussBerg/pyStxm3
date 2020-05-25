5ce911cf14c7e821e0e3a0119997ba9936b64ca4
rb <russ.berg@lightsource.ca>
2020-05-25 10:46:31 -0600

    remove local modules no longer in use

--- cls/epics/devices/stringin.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-import epics  
-
-class stringin(epics.Device):
-    "Simple string input device"
-    _fields = ('setpoint','NAME','DESC','INP')
-    def __init__(self,prefix):
-        if not prefix.endswith('.'): prefix = "%s." % prefix
-        epics.Device.__init__(self,prefix,self._fields)
